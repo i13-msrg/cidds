f014bd45c8265de78a28a56f6278e41a65982257
rayoriz <riswan.lathif@gmail.com>
2018-12-04 15:23:10 +0100

    index changes

--- simulator/urls.py
+++ simulator/urls.py
@@ -5,7 +5,7 @@ from simulator.views import StartSim, SimulationHistory, Comparison
 
 urlpatterns = [
     path('initialize/', StartSim.as_view(), name='initialize'),
-    path('', TemplateView.as_view(template_name='start_form.html')),
+    path('', TemplateView.as_view(template_name='start_form.html'), name='startsim'),
     path('history/', SimulationHistory.as_view(), name='history'),
     path('compare/', Comparison.as_view(), name='compare'),
 
